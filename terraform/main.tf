320cabfca11374e14a9e6f6da96b850709ab402d
ian xhio <xhio.ai@MacBook-Pro-de-Ian.local>
2026-09-06 14:16:52 -0600

    fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    render = {
-      source  = "render-oss/render"
-      version = "~> 1.3.0"
-    }
-  }
-}
-
-provider "render" {
-  api_key  = var.render_api_key
-  owner_id = var.render_owner_id
-}
-
-variable "render_api_key" {
-  type        = string
-  description = "API Key de Render"
-  sensitive   = true
-}
-
-variable "render_owner_id" {
-  type        = string
-  description = "User ID o Team ID de la cuenta en Render"
-  sensitive   = true
-}
-
 resource "render_postgres" "db" {
   name          = "persistent-postgres"
   plan          = "free"
